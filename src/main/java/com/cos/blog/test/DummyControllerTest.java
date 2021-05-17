7cade6615f1e38e5c8d374ca343d8d3b56d095cd
yjh4218 <yjh0950@gmail.com>
2021-05-17 23:32:14 +0900

    2021.05.17 blog update 처리. blog 강좌 28강 완료
    test에서 유저 삭제 완료, jsp 메인 화면, 로그인, 회원가입 화면 구성 완료
    34강까지 완료:

--- src/main/java/com/cos/blog/test/DummyControllerTest.java
+++ src/main/java/com/cos/blog/test/DummyControllerTest.java
@@ -3,6 +3,8 @@ package com.cos.blog.test;
 import java.util.List;
 import java.util.function.Supplier;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -10,6 +12,8 @@ import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.data.domain.*;
 
@@ -25,6 +29,33 @@ public class DummyControllerTest {
 	@Autowired //의존성 주입(DI)
 	private UserRepository userRepository;
 	
+	
+	//save 함수는 id를 전달하지 않으면 insert를 하고
+	//save 함수는 id를 전달하면 해당 id에 대한 데이터가 있으면 update를 진행함
+	//save 함수는 id를 전달하면 해당 id에 대한 데이터가 없으면 insert를 진행함
+	//email, password
+	@Transactional
+	@PutMapping("/dummy/user/{id}")
+	public User updateUser(@PathVariable int id,@RequestBody User requestUser) { //json 데이터를 요청 -> Java ObjectMessageConverter의 Jackson라이브러리가 변환해서 받아줌
+		System.out.println("id : " + id);
+		System.out.println("password : " + requestUser.getPassword());
+		System.out.println("email : " + requestUser.getEmail());
+		
+		// save는 인서트에서만 사용한다.
+		User user = userRepository.findById(id).orElseThrow(()->{
+			return new IllegalArgumentException("수정에 실패하였습니다.");
+		});
+		
+		user.setPassword(requestUser.getPassword());
+		user.setEmail(requestUser.getEmail());
+		
+//		userRepository.save(user); 
+		
+		//더티체킹
+		return null;
+	}
+	
+	
 	@GetMapping("/dummy/users")
 	public List<User> list(){
 		return userRepository.findAll();
@@ -35,10 +66,7 @@ public class DummyControllerTest {
 	public List<User> pageList(@PageableDefault(size=2, sort="id", direction=Sort.Direction.DESC) Pageable pageable){
 		Page<User> pageUser = userRepository.findAll(pageable); 
 		
-		if(pageUser.isFirst()) {
-			
-		}
-		
+				
 		List<User> users = pageUser.getContent(); //getContent()는 List 값으로 반환한다.
 		return users;
 	}
